aa9bc63c71a2e987d1de75a429f8f5ab48e50a6c
Quincy van Deursen <q.vandeursen@student.avans.nl>
2022-02-28 16:01:08 +0100

    Added: Database package and Connection Class.

---
+++ src/Database/DBConnection.java
@@ -0,0 +1,25 @@
+package Database;
+
+import java.sql.*;
+
+public class DBConnection {
+    private static String connectionURL = "jdbc:sqlserver://localhost;databaseName=Codecademy;integratedSecurity=true;";
+    private static String SQLDriver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+
+    // Method to create the connection with the CodeCademy database;
+    // Method is static so no DBConnection object has to be innitialized.
+    // Method is protected to ensure the method can only be used within the Database package.
+    protected static Connection getConnection() {
+        try {
+            Class.forName(SQLDriver);
+            Connection connect = DriverManager.getConnection(connectionURL);
+            return connect;
+
+        } catch (Exception e) {
+            System.out.println("Problem with database connection occured.");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
