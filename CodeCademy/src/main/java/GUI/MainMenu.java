7417834a2d22758fe39af03e22bb714d01e08a09
Quincy van Deursen <q.vandeursen@student.avans.nl>
2022-03-31 15:03:49 +0200

    Changed: Mainmenu (Optimized code and styling)
    removed duplicate code and removed unneeded buttons and labels.

--- CodeCademy/src/main/java/GUI/MainMenu.java
+++ CodeCademy/src/main/java/GUI/MainMenu.java
@@ -1,119 +1,110 @@
 package GUI;
 
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class MainMenu extends Application {
-    private static Label crudLabel = new Label("CRUD");
+import java.util.concurrent.Flow;
+
+public class MainMenu extends Application implements EventHandler {
+
+    private Label crudLabel = new Label("CRUD");
+    private Label statisticsLabel = new Label("Statistieken");
+    private Button studentButton = new Button("Cursisten");
+    private Button enrollButton = new Button("Inschrijvingen");
+    private Button statisticsButton = new Button("Statistieken");
+    private Stage stage;
+
 
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Thomas van Otterloo (2186082) / Quincy van Deursen (2113709)");
-        stage.setScene(getView(stage));
+        stage.setScene(this.getView(stage));
         stage.show();
     }
 
 
     //    Creating a pane for the scene.
-    public Scene getView(Stage window) {
-        GridPane mainPane = getMainPane(window);
-        Scene scene = new Scene(mainPane, 950, 300);
+    public Scene getView(Stage stage) {
+        this.stage = stage;
+        FlowPane flowPane = getMainPane(stage);
+
+        setPaneStyling(flowPane);
+        setButtonStyling(this.enrollButton);
+        setButtonStyling(this.studentButton);
+        setButtonStyling(this.statisticsButton);
+        setLabelStyling(this.crudLabel);
+        setLabelStyling(this.statisticsLabel);
+
+        this.enrollButton.setOnAction(this::handle);
+        this.studentButton.setOnAction(this::handle);
+        this.statisticsButton.setOnAction(this::handle);
+
+
+        Scene scene = new Scene(flowPane, 250, 500);
         return scene;
     }
 
-    //    creating Buttons inside the pane.
-    private GridPane getMainPane(Stage window) {
-        GridPane mainPane = new GridPane();
-
-        Button studentButton = new Button("Cursisten");
-        Button cursusButton = new Button("Cursussen");
-        Button enrollButton = new Button("Inschrijvingen");
-        Button certificateButton = new Button("Certificaten");
-
-        mainPane.add(crudLabel, 0, 0);
-        mainPane.add(studentButton, 0, 1);
-        mainPane.add(cursusButton, 1, 1);
-        mainPane.add(enrollButton, 2, 1);
-        mainPane.add(certificateButton, 3, 1);
-
-        createActionsForButtons(studentButton, cursusButton, enrollButton, certificateButton, window);
-
-        setStyling(mainPane, studentButton, cursusButton, enrollButton, certificateButton);
-
-        return mainPane;
+    //    Creating gridPane and placing buttons and labels.
+    private FlowPane getMainPane(Stage window) {
+        FlowPane flowPane = new FlowPane(Orientation.VERTICAL);
+        flowPane.setColumnHalignment(HPos.CENTER);
+        flowPane.getChildren().addAll(crudLabel, studentButton, enrollButton, statisticsLabel, statisticsButton);
+        return flowPane;
     }
 
-    // Creates styles for everything.
-    private void setStyling(GridPane mainPane, Button studentButton, Button cursusButton, Button enrollButton, Button certificateButton) {
+    // Creates the style for the mainPane.
+    private static void setPaneStyling(FlowPane mainPane) {
         mainPane.setPadding(new Insets(25, 25, 25, 25));
         mainPane.setVgap(50);
         mainPane.setHgap(40);
         mainPane.setStyle("-fx-background-color:#FAF3DC");
+    }
 
-        crudLabel.setStyle("-fx-font-weight: bold;" +
+    //creates the style for the buttons
+    private static void setButtonStyling(Button button) {
+        button.setStyle(
+                "-fx-background-color:#2596be;" +
+                        "-fx-font-size: 2em;" +
+                        " -fx-background-radius: 12;" +
+                        "-fx-effect: dropshadow( gaussian , rgba(0,0,0,5) , 4,0,0,2 );" +
+                        "-fx-font-weight: bold;"
+        );
+    }
+
+    //creates the style for the labels
+    private static void setLabelStyling(Label label) {
+        label.setStyle("-fx-font-weight: bold;" +
                 "-fx-font-size: 2em;");
-
-        studentButton.setStyle(
-                "-fx-background-color:#2596be;" +
-                        "-fx-font-size: 2em;" +
-                        " -fx-background-radius: 12;" +
-                        "-fx-effect: dropshadow( gaussian , rgba(0,0,0,5) , 4,0,0,2 );" +
-                        "-fx-font-weight: bold;"
-        );
-
-        cursusButton.setStyle(
-                "-fx-background-color:#2596be;" +
-                        "-fx-font-size: 2em;" +
-                        " -fx-background-radius: 12;" +
-                        "-fx-effect: dropshadow( gaussian , rgba(0,0,0,5) , 4,0,0,2 );" +
-                        "-fx-font-weight: bold;"
-        );
-
-        enrollButton.setStyle(
-                "-fx-background-color:#2596be;" +
-                        "-fx-font-size: 2em;" +
-                        " -fx-background-radius: 12;" +
-                        "-fx-effect: dropshadow( gaussian , rgba(0,0,0,5) , 4,0,0,2 );" +
-                        "-fx-font-weight: bold;"
-        );
-
-        certificateButton.setStyle(
-                "-fx-background-color:#2596be;" +
-                        "-fx-font-size: 2em;" +
-                        " -fx-background-radius: 12;" +
-                        "-fx-effect: dropshadow( gaussian , rgba(0,0,0,5) , 4,0,0,2 );" +
-                        "-fx-font-weight: bold;"
-        );
     }
 
-    // Handles all SetOnAction buttons.
-    private void createActionsForButtons(Button studentButton, Button cursusButton, Button enrollButton, Button certificateButton, Stage stage) {
-        StudentMenu studentMenu = new StudentMenu();
-        //Opens student menu
-        studentButton.setOnAction(actionEvent -> {
+    //Event handler for the buttons.
+    @Override
+    public void handle(Event event) {
+        if (event.getSource() == studentButton) {
+            StudentMenu studentMenu = new StudentMenu();
             stage.setScene(studentMenu.getView(stage));
-        });
-
-        cursusButton.setOnAction(actionEvent -> {
-//                    window.setScene(naamClassinvullen.getVieuw());
-        });
-
-        enrollButton.setOnAction(actionEvent -> {
-//                    window.setScene(EnrollmentMenu.getVieuw());
-        });
-
-        certificateButton.setOnAction(actionEvent -> {
-//                    window.setScene(naamClassinvullen.getVieuw());
-        });
+            return;
+        }
+//       if (event.getSource() == enrollButton){
+//             EnrollMenu enrollMenu = new EnrollMenu();
+//            stage.setScene(enrollMenu.getView(stage));
+//            return;
+//        }
+//        if (event.getSource() == statisticsButton){
+//             StatisticsMenu statisticsMenu = new StatisticsMenu();
+//            stage.setScene(statisticsMenu.getView(stage));
+//        }
     }
-
-    public void loadMainMenu(){
-
-    }
-
 }
