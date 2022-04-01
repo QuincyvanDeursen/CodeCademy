23ed0d10132688e6d537c04aba651f0fb85dcaad
Thomas van Otterloo <vanotterloo.thomas@gmail.com>
2022-04-01 16:56:50 +0200

    Enums* Added a return method.

--- CodeCademy/src/main/java/Domain/Level.java
+++ CodeCademy/src/main/java/Domain/Level.java
@@ -1,5 +1,18 @@
 package Domain;
 
 public enum Level {
-    BEGINNER, ADVANCED, EXPERT
+    BEGINNER, ADVANCED, EXPERT;
+
+    public static Level returnEnum(String value) {
+        if (value.equals("BEGINNER")) {
+            return Level.BEGINNER;
+        }
+        if (value.equals("ADVANCED")){
+            return Level.ADVANCED;
+        }
+        if (value.equals("EXPERT")){
+            return Level.EXPERT;
+        }
+        return null;
+    }
 }
--- CodeCademy/src/main/java/Domain/Status.java
+++ CodeCademy/src/main/java/Domain/Status.java
@@ -3,5 +3,18 @@ package Domain;
 public enum Status {
     CONCEPT,
     ARCHIVED,
-    ACTIVE
+    ACTIVE;
+
+    public static Status returnEnum(String value) {
+        if (value.equals("CONCEPT")) {
+            return Status.CONCEPT;
+        }
+        if (value.equals("ARCHIVED")){
+            return Status.ARCHIVED;
+        }
+        if (value.equals("ACTIVE")){
+            return Status.ACTIVE;
+        }
+        return null;
+    }
 }
