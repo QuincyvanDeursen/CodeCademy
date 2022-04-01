b336c54de6e7d6cfc215ffba56b22e715f85129d
Quincy van Deursen <q.vandeursen@student.avans.nl>
2022-04-01 16:12:07 +0200

    Corrected fault in Module and Webcast domain

--- CodeCademy/src/main/java/Domain/Module.java
+++ CodeCademy/src/main/java/Domain/Module.java
@@ -5,12 +5,14 @@ import java.util.Date;
 public class Module extends ContentItem{
     private double version;
     private int serialNumber;
+    private ContactPersonModule contactPersonModule;
 
     //Constructor
-    public Module(int ID, String title, Date publicationDate, Status status, double version, int serialNumber) {
+    public Module(int ID, String title, Date publicationDate, Status status, double version, int serialNumber, ContactPersonModule contactPersonModule) {
         super(ID, title, publicationDate, status);
         this.version = version;
         this.serialNumber = serialNumber;
+        this.contactPersonModule = contactPersonModule;
     }
 
     //Getters and Setters
--- CodeCademy/src/main/java/Domain/Webcast.java
+++ CodeCademy/src/main/java/Domain/Webcast.java
@@ -5,12 +5,14 @@ import java.util.Date;
 public class Webcast extends ContentItem{
     private String url;
     private int duration;
+    private SpeakerWebcast speakerWebcast;
     
     //Constructor
-    public Webcast(int ID, String title, Date publicationDate, Status status, String url, int duration) {
+    public Webcast(int ID, String title, Date publicationDate, Status status, String url, int duration, SpeakerWebcast speaker) {
         super(ID, title, publicationDate, status);
         this.url = url;
         this.duration = duration;
+        this.speakerWebcast = speaker;
     }
 
     //Getters and settesr
